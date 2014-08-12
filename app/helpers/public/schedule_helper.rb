066ff5bc981f39baeb568b399e9f7495c7fba935
Matthias Schiffer <mschiffer@universe-factory.net>
2014-08-12 02:21:26 +0200

    Split public schedule at separators

--- app/helpers/public/schedule_helper.rb
+++ app/helpers/public/schedule_helper.rb
@@ -2,8 +2,36 @@ module Public::ScheduleHelper
 
   require 'scanf'
 
+  def day_parts
+    separators = @day.day_separators.collect { |sep| sep.time }
+
+    starts = Array.new(separators)
+    starts.unshift @day.start_date
+
+    ends = Array.new(separators)
+    ends << @day.end_date
+
+    starts.zip(ends)
+  end
+
   def each_timeslot(&block)
     each_minutes(@conference.timeslot_duration, &block)
+  end
+
+  def each_part_timeslot((start_time, end_time), &block)
+    time = start_time
+    while time < end_time
+      yield time
+      time = time.since(@conference.timeslot_duration.minutes)
+    end
+  end
+
+  def used_rooms((start_time, end_time))
+    rooms = Array.new @conference.rooms.public
+
+    rooms.keep_if do |room|
+      @events[room].any? { |event| event.start_time >= start_time and event.start_time < end_time }
+    end
   end
 
   def color_dark?(color)
